8cf1fd6205fecc0ba8a5e975930f036383ec4045
Dirk Arnez <smalldirkalex@gmail.com>
2026-07-21 18:58:42 +0800

    - update src/init.c

--- src/init.c
+++ src/init.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
- 
+#include <sys/mount.h>
+#include <stddef.h>
+
+void init() {
+	 mount("proc", "/target/path/proc", "proc", 0, NULL);
+}
+
 void main() {
+	init();
+	
 	char buff[100];
 	printf("Enter anything");
 	while(1)
